3670e657cb021b224051fd04d08b94da3c3f30dc
sandeep.kumar@adsandurl.com <sandeep.kumar@adsandurl.com>
2021-07-24 11:40:37 +0530

    dependency

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\learning\Clima-Flutter"
+export "FLUTTER_ROOT=C:\project_software\flutter"
+export "FLUTTER_APPLICATION_PATH=S:\Project\Clima-Flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
